272ccacd4b1963c0bf3b2d8a885ab2e0bd210665
DerpyBurgerz <s.h.eikelenboom@timetkonow.nl>
2025-11-25 11:16:30 +0100

    This is the last commit before I change the whole thing to a vec<Option<Node<T>>>

--- src/datastructures/linked_vector.rs
+++ src/datastructures/linked_vector.rs
@@ -8,30 +8,53 @@ pub struct CompactLinkedVector<T> {
     tail: Option<NodeIndex>, // the index of the tail in our list
 }
 impl<T> LinkedVector<T> for CompactLinkedVector<T>{
-    fn get_random<R>(&self, rng: &mut R) -> Option<&Node<T>>
+    fn get_random<R>(&self, rng: &mut R) -> Option<(NodeIndex, &T)>
     where
         R: Rng + ?Sized
     {
-        self.list.choose(rng)
+         if let Some(node) = self.list.choose(rng){
+             Some((node.index, &node.value))
+         } else {
+             None
+         }
     }
 
-    fn get_at_index(&self, i: usize) -> Option<&Node<T>> {
-        if i < self.list.len(){
-            return Some(&self.list[i]);
+    fn get_value(&self, index: NodeIndex) -> Option<&T> {
+        if index < self.list.len(){
+            let node = &self.list[index];
+            Some(&node.value)
+        } else {
+            None
         }
-        None
     }
 
-    fn get_head(&self) -> Option<&Node<T>> {
-        Some(&self.list[self.head?])
+    fn get_mut_value(&mut self, index: NodeIndex) -> Option<&mut T> {
+        if index < self.list.len(){
+            let node = &mut self.list[index];
+            Some(&mut node.value)
+        } else {
+            None
+        }
     }
 
-    fn get_tail(&self) -> Option<&Node<T>> {
-        Some(&self.list[self.tail?])
+    fn get_head_index(&self) -> Option<NodeIndex> {
+        if self.head.is_none(){
+            return None;
+        }
+        let node = &self.list[self.head.unwrap()];
+        Some(node.index)
     }
-    fn insert_after(&mut self, index: NodeIndex, value: T) -> &Node<T> {
+
+    fn get_tail_index(&self) -> Option<NodeIndex> {
+        if self.tail.is_none(){
+            return None;
+        }
+        let node = &self.list[self.tail.unwrap()];
+        Some(node.index)
+    }
+    fn insert_after(&mut self, index: NodeIndex, value: T) -> NodeIndex {
         if index > self.list.len() { panic!("tried to index out of range")}
-        let node = self.get_at_index(index).unwrap();
+        let node = &self.list[index];
 
         if let Some(next_index) = node.next{
             self.insert_(Some(next_index), value)
@@ -40,21 +63,28 @@ impl<T> LinkedVector<T> for CompactLinkedVector<T>{
         }
     }
 
-    fn insert_before(&mut self, index: NodeIndex, value: T) -> &Node<T> {
+    fn insert_before(&mut self, index: NodeIndex, value: T) -> NodeIndex {
         if index > self.list.len() { panic!("tried to index out of range")}
-        let node = self.get_at_index(index).unwrap();
+        let node = &self.list[index];
         self.insert_(Some(node.index), value)
     }
 
-    fn push_front(&mut self, value: T) -> &Node<T> {
+    fn push_front(&mut self, value: T) -> NodeIndex {
         self.insert_(self.head, value)
     }
 
-    fn push_back(&mut self, value: T) -> &Node<T> {
+    fn push_back(&mut self, value: T) -> NodeIndex{
         self.insert_(None, value)
     }
 
-    fn remove(&mut self, node: Node<T>, value: T) -> &Node<T> {
+    fn remove(&mut self, index: NodeIndex) {
+        let node = &self.list[index];
+        if let Some(prev) = node.prev{
+            self.list[prev].next = node.next;
+        } else {
+            // there is no previous index
+            // we are at the start of the linkedlist
+        }
         todo!()
     }
 }
@@ -71,7 +101,7 @@ impl<T> CompactLinkedVector<T> {
     /// If node is Some(node) we add the value in front of the node in the linkedlist.
     /// If node is None, we add the value to the end of the linked list
     /// In all cases, the value will be put at the end of the vector
-    fn insert_(&mut self, node_index: Option<usize>, value: T) -> &Node<T>{
+    fn insert_(&mut self, node_index: Option<usize>, value: T) -> NodeIndex{
         let new_index = self.list.len();
         if self.list.is_empty(){
             #[cfg(debug_assertions)]
@@ -85,8 +115,6 @@ impl<T> CompactLinkedVector<T> {
             self.head = Some(0);
             self.tail = Some(0);
             self.list.push(new_node);
-
-            &self.list[0]
         } else if let Some(node) = node_index {
             let prev = self.list[node].prev;
             self.list[node].prev = Some(new_index);
@@ -104,7 +132,6 @@ impl<T> CompactLinkedVector<T> {
                 next: Some(node),
             };
             self.list.push(new_node);
-            &self.list[new_index]
         } else { // if node is None
             if let Some(tail) = self.tail{
                 let tail = &mut self.list[tail];
@@ -121,8 +148,8 @@ impl<T> CompactLinkedVector<T> {
 
             self.tail = Some(new_index);
             self.list.push(new_node);
-            &self.list[new_index]
         }
+        new_index
     }
 }
 #[cfg(test)]
@@ -133,27 +160,23 @@ mod tests{
         let mut lv = CompactLinkedVector::new();
         lv.push_back(1);
         assert_eq!(lv.head.unwrap(), 0, "yay");
-        assert_eq!(lv.get_head().unwrap().value, 1, "yay");
+        assert_eq!(lv.list[lv.head.unwrap()].value, 1, "yay");
+        assert_eq!(lv.list[lv.tail.unwrap()].value, 1, "yay");
     }
     #[test]
     fn push_front_to_empty_list(){
         let mut lv = CompactLinkedVector::new();
         lv.push_front(1);
         assert_eq!(lv.head.unwrap(), 0, "yay");
-        assert_eq!(lv.get_head().unwrap().value, 1, "yay");
+        assert_eq!(lv.list[lv.head.unwrap()].value, 1, "yay");
+        assert_eq!(lv.list[lv.tail.unwrap()].value, 1, "yay");
     }
     #[test]
     fn insert(){
         let mut lv = CompactLinkedVector::new();
         let node1 = lv.push_back(1);
         let node2 = lv.push_back(2);
-
-        let before2 = *lv.insert_before(node2.index, 2);
-
-        lv.insert_before(node1.index, 100);
-        assert_eq!(lv.get_head().unwrap().value, 100);
-        assert_eq!(lv.get_head().unwrap().next.unwrap(), before2.index);
-        assert_eq!(lv.get_tail().unwrap().value, 5);
-
+        assert_eq!(lv.list[lv.head.unwrap()].value, 1, "yay");
+        assert_eq!(lv.list[lv.tail.unwrap()].value, 2, "yay");
     }
 }
--- src/datastructures/linked_vectors.rs
+++ src/datastructures/linked_vectors.rs
@@ -1,21 +1,21 @@
 use rand::{Rng, RngCore};
 
-pub trait LinkedVector<T> {
-    fn get_random<R>(&self, rng: &mut R) -> Option<&Node<T>> where R:Rng + ?Sized;
-    fn get_at_index(&self, index: NodeIndex) -> Option<&Node<T>>;
-    fn get_head(&self) -> Option<&Node<T>>;
-    fn get_tail(&self) -> Option<&Node<T>>;
-    fn insert_after(&mut self, index: NodeIndex, value: T) -> &Node<T>;
-    fn insert_before (&mut self, index: NodeIndex, value: T) -> &Node<T>;
-    fn push_front(&mut self, value: T) -> &Node<T>;
-    fn push_back(&mut self, value: T) -> &Node<T>;
-    fn remove(&mut self, node: Node<T>, value: T) -> &Node<T>;
+pub trait LinkedVector<T>{
+    fn get_random<R>(&self, rng: &mut R) -> Option<(NodeIndex, &T)> where R:Rng + ?Sized;
+    fn get_value(&self, index: NodeIndex) -> Option<&T>;
+    fn get_mut_value(&mut self, index: NodeIndex) -> Option<&mut T>;
+    fn get_head_index(&self) -> Option<NodeIndex>;
+    fn get_tail_index(&self) -> Option<NodeIndex>;
+    fn insert_after(&mut self, index: NodeIndex, value: T) -> NodeIndex;
+    fn insert_before (&mut self, index: NodeIndex, value: T) -> NodeIndex;
+    fn push_front(&mut self, value: T) -> NodeIndex;
+    fn push_back(&mut self, value: T) -> NodeIndex;
+    fn remove(&mut self, index: NodeIndex);
 }
-#[derive(Copy, Clone)]
 pub struct Node<T>{
     pub value: T,
-    pub index: usize,
-    pub prev: Option<usize>,
-    pub next: Option<usize>,
-}
+    pub index: NodeIndex,
+    pub prev: Option<NodeIndex>,
+    pub next: Option<NodeIndex>,
+    }
 pub type NodeIndex = usize;
