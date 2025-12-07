e4110cf490d19d04d7c6720b754c8704c1370d66
DerpyBurgerz <s.h.eikelenboom@timetkonow.nl>
2025-12-07 21:45:42 +0100

    starting on fixplzplzpzpl

--- src/simulated_annealing/FIXTHISSHITANDWEAREDONE.rs
+++ src/simulated_annealing/FIXTHISSHITANDWEAREDONE.rs
@@ -1,5 +1,25 @@
+use std::collections::HashMap;
+use crate::get_orders;
+use crate::simulated_annealing::day::Day;
+use crate::simulated_annealing::route::Route;
 use crate::simulated_annealing::week::Week;
 
 pub fn fixplzplzplzpl(truck1: &mut Week, truck2: &mut Week){
+    let orders = get_orders();
+    let mut order_count:HashMap<usize, usize> = HashMap::new();
+
+}
+
+
+pub fn count_per_week(truck: & Week){
+
+}
+pub fn count_per_day(day: &Day){
+
+}
+pub fn count_per_route(day: &Route, order_count: HashMap<usize, usize>){
+    for node in day.linked_vector.iter(){
+
+    }
 
 }
