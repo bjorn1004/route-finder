c9cf41b9ae80d3e33e312358f66a7fbacb3cef5f
DerpyBurgerz <s.h.eikelenboom@timetkonow.nl>
2025-12-20 12:28:25 +0100

    Solution is now clonable

--- src/simulated_annealing/solution.rs
+++ src/simulated_annealing/solution.rs
@@ -1,6 +1,7 @@
 use crate::simulated_annealing::week::Week;
 use crate::simulated_annealing::score_calculator::{calculate_score};
 
+#[derive(Clone)]
 pub struct Solution{
     pub truck1: Week,
     pub truck2: Week,
